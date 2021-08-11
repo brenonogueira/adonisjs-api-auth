47ed5bcc1af47cf00c3d7c612846b5fd706c9932
Breno Nogueira Araújo <breno.nog.araujo@hotmail.com>
2021-08-11 00:08:23 -0400

    Construindo API com Auth

---
+++ app/Controllers/Http/UsersController.ts
@@ -0,0 +1,31 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User'
+
+
+export default class UsersController {
+  public async index ({}: HttpContextContract) {
+    const user = await User.all()
+    return user;
+  }
+
+  public async register ({ request, response }: HttpContextContract) {
+    const dados = request.all();
+    const users = await User.create(dados);
+    return response.status(201).send(users)
+  }
+
+  public async store ({}: HttpContextContract) {
+  }
+
+  public async show ({}: HttpContextContract) {
+  }
+
+  public async edit ({}: HttpContextContract) {
+  }
+
+  public async update ({}: HttpContextContract) {
+  }
+
+  public async destroy ({}: HttpContextContract) {
+  }
+}
